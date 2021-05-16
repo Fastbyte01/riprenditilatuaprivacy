f56808b9150a38fc0d80828a8ebfc881aa923fa2
Giuseppe Pignataro <rogepix@gmail.com>
2021-05-16 23:29:24 +0200

    Modificati collegamenti al repository

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,7 +53,7 @@ module.exports = {
           items: [
             {
               label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              href: 'https://twitter.com/fastbyte01',
             },
           ],
         },
@@ -82,13 +82,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/Fastbyte01/riprenditilatuaprivacy/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/Fastbyte01/riprenditilatuaprivacy/edit/master/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
